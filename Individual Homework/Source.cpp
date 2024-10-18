499fc7db53248a0bf0244a3242ef937949d22c3e
Doholod <egorcherd16@gmail.com>
2024-10-18 22:18:28 +1000

    Добавил файлы для data

--- Individual Homework/Source.cpp
+++ Individual Homework/Source.cpp
@@ -1,8 +1,9 @@
-/// Сделал Черданцев Егор 
+/// Сделал Черданцев Егор ///
 
 #include <iostream>
 #include <math.h>
-#include <math.h>
+#include <fstream>
+#include <string>
 using namespace std;
 
 //Задание 1
@@ -16,15 +17,52 @@ using namespace std;
 //}
 
 //Задание 2
-#define a2 (a*a)
-#define a4 (a2*a2)
-#define a5 (a4*a)
-#define a10 (a5*a5)
-#define a15 (a10*a5)
-int main() {
-	long double a; cin >> a;
-	cout.precision(5);
-	cout << a15;
-}
+//#define a2 (a*a)
+//#define a4 (a2*a2)
+//#define a5 (a4*a)
+//#define a10 (a5*a5)
+//#define a15 (a10*a5)
+//int main() {
+//	long double a; cin >> a;
+//	cout.precision(5);
+//	cout << a15;
+//}
 
 //Задание 3
+int main() {
+	setlocale(0, "RU");
+	//Задача 1
+	long double b, q; cin >> b >> q;
+	long long n; cin >> n;
+	cout << "Задача 1, ответ: " << b*(1- pow(q, n)) / (1-q)<<endl;
+
+	//Задача 2
+	ifstream infile;
+	infile.open("in2.dat", ios::in);
+
+	//Считываем данные из файла
+	infile >> b;
+	infile.ignore(10000, ' ');
+	infile >> q;
+	infile.ignore(10000, ' ');
+	infile >> n;
+	infile.close();
+	cout << "Задача 2, ответ: " << b * (1 - pow(q, n)) / (1 - q) << endl;
+
+	//Задача 3
+	infile.open("in3.dat", ios::in);
+
+	//Считываем данные из файла
+	infile >> b;
+	infile.ignore(10000, ' ');
+	infile >> q;
+	infile.ignore(10000, ' ');
+	infile >> n;
+
+	//Вывод результата в файл "out3.data"
+	infile.close();
+	ofstream outfile;
+	outfile.open("out3.dat");
+	outfile.clear();
+	outfile << "Задача 3, ответ: " << b * (1 - pow(q, n)) / (1 - q) << endl;
+}
